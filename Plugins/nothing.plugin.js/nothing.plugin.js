561492c990d8a808fac403d599f885e3bfd21fbd
Dark-Lynn <65414822+Dark-LYNN@users.noreply.github.com>
2022-02-24 20:19:50 +0100

    Update nothing.plugin.js

--- Plugins/nothing.plugin.js/nothing.plugin.js
+++ Plugins/nothing.plugin.js/nothing.plugin.js
@@ -1,5 +1,15 @@
-/* MIT License */
+/*
+* @name nothing
+* @description This plugin does nothing.
+* @author Dark-LYNN
+* @authorId 705306248538488947
+* @version 1.0.0
+* @source https://github.com/Dark-LYNN/BetterDiscord/tree/v1.0.0/Plugins/nothing.plugin.js
+* @updateUrl https://github.com/Dark-LYNN/BetterDiscord/tree/v1.0.0/Plugins/nothing.plugin.js/nothing.plugin.js
+* @license MIT
+*/
 
+/* MIT License */
 class Nothing {
   getName() {
     return "Nothing";
